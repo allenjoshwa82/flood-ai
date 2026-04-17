69be10e2c8084faeafb0aa438ceb3c9b89272090
allenjoshwa82 <mgkrishna.msc@gmail.com>
2026-04-17 11:17:35 +0530

    Update app.py

--- flood_prediction/app.py
+++ flood_prediction/app.py
@@ -10,7 +10,7 @@ from tensorflow.keras.models import load_model
 app = Flask(__name__)
 
 # Load model
-model = load_model("model.h5")
+model = load_model("model.h5", compile=False)
 scaler = pickle.load(open("scaler.pkl", "rb"))
 columns = pickle.load(open("columns.pkl", "rb"))
 
