685bb07b65a61d87d85ab3d324a6e7313629b4c8
allenjoshwa82 <mgkrishna.msc@gmail.com>
2026-04-20 14:59:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ outputs = Dense(1, activation='sigmoid')(x)
 model = Model(inputs, outputs)
 
 # ✅ Load weights only (NO config loading)
-model.load_weights("model.weights.h5")
+
 
 # Load scaler
 scaler = pickle.load(open("scaler.pkl", "rb"))
